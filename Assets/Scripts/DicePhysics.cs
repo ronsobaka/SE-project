e47fc6bf85c507ac6390af762933f6f26d53621d
ronsobaka <75744766+ronsobaka@users.noreply.github.com>
2025-02-06 16:51:37 +0000

    Dice Roll 2

--- Assets/Scripts/DicePhysics.cs
+++ Assets/Scripts/DicePhysics.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DicePhysics : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class DicePhysics : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	static Rigidbody rb;
+	public static Vector3 diceVelocity;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		diceVelocity = rb.velocity;
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			float dirX = Random.Range (0, 100);
+			float dirY = Random.Range (0, 100);
+			float dirZ = Random.Range (0, 100);
+			transform.rotation = Quaternion.identity;
+			rb.AddForce (transform.up * 200);
+            rb.AddForce (transform.forward * 100);
+			rb.AddTorque (dirX, dirY, dirZ);
+		}
+	}
 }
