8442e00072a417c70716599925132648802e25de
Ccampbell2003 <Christianpaulcampbell@gmail.com>
2025-02-28 00:36:27 +0000

    Card Display
    Have added correct card display for each property colour so far
    -have 2 buttons for buy and auction
    -buy closes the popup
    -issues that need sorting: when pressing buy the popup for the same card shows again so need to create a bool which keeps track if the card pop up is open at the time

--- Assets/Scripts/EndOfTurnActions.cs
+++ Assets/Scripts/EndOfTurnActions.cs
@@ -25,13 +25,15 @@ public class EndOfTurnActions : MonoBehaviour
         GameController.turnActionsTrigger = false;
         playerPositions = GameController.playerPositions;
         currentPlayer = GameController.currentPlayer;
-        //staticDecisionOutput.enabled = true;  
+        //staticDecisionOutput.enabled = true;
 
-        if (GameController.boardData[playerPositions[currentPlayer], 2] != ""){
+        string currentTileData = GameController.boardData[playerPositions[currentPlayer], 2];  
+
+        if (currentTileData != ""){
 
             //staticDecisionOutput.text = "You landed on " + GameController.boardData[playerPositions[currentPlayer], 1] + " This costs " + GameController.boardData[playerPositions[currentPlayer], 5] + "<br><br>Press b to buy or a to auction.";
             PopUps pop = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PopUps>();
-            pop.popUpCard();
+            pop.popUpCard(currentTileData);
         }
     }
 }
--- Assets/Scripts/PopUps.cs
+++ Assets/Scripts/PopUps.cs
@@ -1,14 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class PopUps : MonoBehaviour  {
     
     public GameObject popUpBox;
     public Animator animator;
+    public Image cardImage;
+    public Sprite[] cardSprites;
+    private Dictionary<string, Sprite> cardDictionary;
+    
 
-    public void popUpCard() {
+    public void Start() {
+        
+        cardDictionary = new Dictionary<string, Sprite> {
+            { "Brown", cardSprites[0] },
+            { "Blue", cardSprites[1] },
+            { "Purple", cardSprites[2] },
+            { "Orange", cardSprites[3] },
+            { "Red", cardSprites[4] },
+            { "Yellow", cardSprites[5] },
+            { "Green", cardSprites[6] },
+            { "Deep blue", cardSprites[7] }
+        };
+    }
+
+
+
+    public void popUpCard(string propertyType) {
+
+        Debug.Log(propertyType);
+
+        if (cardDictionary.ContainsKey(propertyType)) {
+            cardImage.sprite = cardDictionary[propertyType];
+        } else {
+            Debug.LogWarning("Property type not found in dictionary: " + propertyType);
+        }
+
         popUpBox.SetActive(true);
         animator.SetTrigger("pop");
     }
