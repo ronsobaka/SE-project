40f9faa32bce64aa3ba68d270769fcc1abd4e21f
ronsobaka <75744766+ronsobaka@users.noreply.github.com>
2025-03-02 10:23:18 +0000

    Fully added Station and Utility descriptions
    - Realised button is only clickable on one half???

--- Assets/Scripts/DiceRoll.cs
+++ Assets/Scripts/DiceRoll.cs
@@ -152,7 +152,7 @@ public class DiceRoll : MonoBehaviour {
 			doubleRolled = true;
 		}
 		diceTotal = dice1Result + dice2Result;
-		diceTotal = 5;
+		diceTotal = 12;
 		diceDetectTrigger = false;
 
 
--- Assets/Scripts/PopUps.cs
+++ Assets/Scripts/PopUps.cs
@@ -59,19 +59,24 @@ public class PopUps : MonoBehaviour  {
 
         propertyName.text = boardData[tileNumber, 1];
         propertyCost.text = "Price: £" + boardData[tileNumber, 5];
-        //propertyCost.Transform.position.y = 35;
+       
         rentPrices.text = "";
         constructionCost.text = "";
 
         if (boardData[tileNumber,2] == "Station"){
 
-            stationAndUtilityPrices.text = "If 1 station is owned rent is £25\nIf 2 stations are owned rent is £50\nIf 3 stations are owned rent is £100\nIf 4 stations are owned rent is £200";
-            //propertyCost.transform.position.y = 10;
+            stationAndUtilityPrices.text = "If 1 station is owned, rent is £25\nIf 2 stations are owned, rent is £50\nIf 3 stations are owned, rent is £100\nIf 4 stations are owned, rent is £200";
+            propertyCost.rectTransform.localPosition = new Vector3(0, 5, 0);
 
+        } else if ((boardData[tileNumber, 2] == "Tesla Utility") || (boardData[tileNumber, 2] == "Edison Utility")) {
+
+            stationAndUtilityPrices.text = "If 1 utility owned, rent is 4 times the dice result\n If 2 utilites are owned, rent is 10 times the dice result";
+            propertyCost.rectTransform.localPosition = new Vector3(0, 5, 0);
 
         } else {
             rentPrices.text = "Unimproved rent: £" + boardData[tileNumber, 6] + "\n1 House rent: £" + boardData[tileNumber, 7] + "\n2 House rent: £" + boardData[tileNumber, 8] + "\n3 House rent: £" + boardData[tileNumber, 9] + "\n4 House rent: £" + boardData[tileNumber, 10]+ "\nHotel rent: £" + boardData[tileNumber, 11];
             constructionCost.text = "House cost £" + boardData[tileNumber, 12] + "\nHotel cost £" + boardData[tileNumber, 12] + " + 4 houses";  
+            propertyCost.rectTransform.localPosition = new Vector3(0, 35, 0);
         }
 
         
